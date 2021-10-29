19baf72764ee124e28c20ac3d03fc6633f4a05af
Saliha <saliha@Salihas-MacBook-Air.local>
2021-10-30 00:25:00 +0900

    Updates

---
+++ precache-manifest.ac2989d984ef2ee76af9e97f5cd834a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cceaa0687501e62d268695fa4bf68f90",
+    "url": "/md-portfolio/index.html"
+  },
+  {
+    "revision": "40057feac7e29101a62e",
+    "url": "/md-portfolio/static/css/2.ead6637f.chunk.css"
+  },
+  {
+    "revision": "d70366c0a84e6486e4cd",
+    "url": "/md-portfolio/static/css/main.099ccacb.chunk.css"
+  },
+  {
+    "revision": "40057feac7e29101a62e",
+    "url": "/md-portfolio/static/js/2.26618c6b.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/md-portfolio/static/js/2.26618c6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d70366c0a84e6486e4cd",
+    "url": "/md-portfolio/static/js/main.cdf365d1.chunk.js"
+  },
+  {
+    "revision": "644f70a13a26d66d2abc",
+    "url": "/md-portfolio/static/js/runtime-main.a3085b73.js"
+  }
+]);
